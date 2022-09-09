5c47b0c7f64b949396ba69438e6058ea4f631dcb
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2022-09-10 00:04:26 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+
+# (c) Shrimadhav U K | Modifieded By @LISA_FAN_LK
+
+import os
+from config import Config
+from pyrogram import Client as Clinton
+
+if __name__ == "__main__" :
+    # create download directory, if not exist
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
+    plugins = dict(root="plugins")
+    Warrior = Clinton("@UploadLinkToFileBot",
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    plugins=plugins)
+    Warrior.run()
