c0e841a889639170805a5adf6a6841dd2e9bff2e
Indian <82756356+jobin00@users.noreply.github.com>
2022-09-11 09:58:51 +0530

    Update help_Nekmo_ffmpeg.py

--- helper_funcs/help_Nekmo_ffmpeg.py
+++ helper_funcs/help_Nekmo_ffmpeg.py
@@ -1,7 +1,7 @@
 
 # (c) Shrimadhav U K
 
-# the logging things
+
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -44,17 +44,11 @@ async def place_water_mark(input_file, output_file, water_mark_file):
         "-i", input_file,
         "-i", watermarked_file,
         "-filter_complex",
-        # https://stackoverflow.com/a/16235519
-        # "\"[0:0] scale=400:225 [wm]; [wm][1:0] overlay=305:0 [out]\"",
-        # "-map \"[out]\" -b:v 896k -r 20 -an ",
         "\"overlay=(main_w-overlay_w):(main_h-overlay_h)\"",
-        # "-vf \"drawtext=text='@FFMovingPictureExpertGroupBOT':x=W-(W/2):y=H-(H/2):fontfile=" + Config.FONT_FILE + ":fontsize=12:fontcolor=white:shadowcolor=black:shadowx=5:shadowy=5\"",
         output_file
     ]
-    # print(commands_to_execute)
     process = await asyncio.create_subprocess_exec(
         *commands_to_execute,
-        # stdout must a pipe to be accessible as process.stdout
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
     )
@@ -95,7 +89,6 @@ async def take_screen_shot(video_file, output_directory, ttl):
     else:
         return None
 
-# https://github.com/Nekmo/telegram-upload/blob/master/telegram_upload/video.py#L26
 
 async def cult_small_video(video_file, output_directory, start_time, end_time):
     # https://stackoverflow.com/a/13891070/4723940
