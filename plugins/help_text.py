1e8bf0ca53daa6672c21dfaae0e1273282c62b50
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2022-09-26 14:54:33 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -127,6 +127,10 @@ async def start(bot, update):
 
 @Clinton.on_message(filters.private & filters.command(["info"]))
 async def add_info_help(bot, update):
+    if update.from_user.last_name:
+        last_name = update.from_user.last_name
+    else:
+        last_name = "None"
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.INFO_TEXT.format(update.from_user.first_name, last_name, update.from_user.username, update.from_user.id, update.from_user.mention, update.from_user.dc_id, update.from_user.language_code, update.from_user.status),
