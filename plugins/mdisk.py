b3111ae9060edeba85af33652e57bf5a579b8a8d
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2022-10-31 01:15:37 +0530

    Update mdisk.py

--- plugins/mdisk.py
+++ plugins/mdisk.py
@@ -1,2 +1,6 @@
-# not available in this repo use @UploadLinkToFileBot this bot support mdisk link ✅💯
-
+# Not available in this repo use @UploadLinkToFileBot this bot support mdisk link ✅💯
+# Only paid user support mdisk link ✅
+# Per month just $0.50/₹10
+# Bot username @UploadLinkToFileBot
+# Check bot /plans and upgrade your premium subscription
+# Please donate you can send any amount
