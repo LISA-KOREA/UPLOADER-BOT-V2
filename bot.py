9665f410c53c216abfe4cf1c0a4cb08a0dabe6a8
JOBIN <82756356+jobin00@users.noreply.github.com>
2022-12-04 10:46:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from config import Config
 from pyrogram import Client as Clinton
 
 if __name__ == "__main__" :
-    # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
