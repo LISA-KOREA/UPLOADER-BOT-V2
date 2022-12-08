a3eec29566fcd7f12a297846f61eff4c93a2f87a
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2022-12-08 17:05:33 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -21,10 +21,6 @@ class Database:
         user = await self.col.find_one({'id': int(id)})
         return True if user else False
 
-    async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
-
     async def get_all_users(self):
         all_users = self.col.find({})
         return all_users
